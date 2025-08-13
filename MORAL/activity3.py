6fdf388f8c62e2c996ce0ee742574216e9f02ba4
med4rda <paulomoral41@gmail.com>
2025-08-13 15:12:37 +0800

    Add files via upload

---
+++ MORAL/activity3.py
@@ -0,0 +1,6 @@
+
+print("\t\t\t*\n \t\t*\t\t*\n\t*\t\t\t\t*\n*\t\tHi,\vpaulopogi\t\t\t*\n\t*\t\t\t\t*\n\t\t*\t\t*\n\t\t\t*")
+print("\t\t\t\t\t\t\t\t\t\t*\n\t\t\t\t\t\t\t\t\t*\t\t*\n\t\t\t\t\t\t\t\t*\t\t\t\t*\n\t\t\t\t\t\t\t*\t\t\t\t\t\t*\n\t\t\t\t\t\t*\t\thi,\vpaulopogi\t\t\t\t\t*\n\t\t\t\t\t\t\t*\t\t\t\t\t\t\n\t\t\t\t\t\t\t\t*\t\t\t\t\t*\n\t\t\t\t\t\t\t\t\t*\t\t\t*\n\t\t\t\t\t\t\t\t\t\t*\t*")
+
+
+
