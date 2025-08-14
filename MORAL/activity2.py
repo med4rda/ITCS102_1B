343febccb1f1d08dbc469f4724f081a405489e93
med4rda <paulomoral41@gmail.com>
2025-08-14 23:27:56 +0800

    Delete MORAL/activity2.py

--- MORAL/activity2.py
+++
@@ -1,3 +0,0 @@
-print("\t\t\t\t\t\t\t\t\t\t*\n\t\t\t\t\t\t\t\t\t*\t\t*\n\t\t\t\t\t\t\t\t*\t\t\t\t*\n\t\t\t\t\t\t\t*\t\t\t\t\t\t*\n\t\t\t\t\t\t*\t\t\thi,\t\t\t\t\t*\n\t\t\t\t\t\t\t\t\t   paulopogi\t\t\t\t\t\n\t\t\t\t\t\t\t*\t\t\t\t\t\t*\n\t\t\t\t\t\t\t\t*\t\t\t\t*\n\t\t\t\t\t\t\t\t\t*\t\t*\n\t\t\t\t\t\t\t\t\t\t*")
-
-
