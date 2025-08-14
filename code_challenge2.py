a1b96a669105eeac82fd9670d80f02d4fda4cd58
med4rda <paulomoral41@gmail.com>
2025-08-14 15:14:21 +0800

    Add files via upload

---
+++ code_challenge2.py
@@ -0,0 +1,30 @@
+x = int(input("Eneter the amount to deposit : "))
+print ("Here is the fucking breakdown of your fucking deposited amount: ")
+y = x
+a1 = y // 1000
+y = y % 1000
+a2 = y // 500
+y = y % 500
+a3 = y // 200
+y = y % 200
+a4 = y // 100
+y = y % 100
+a5 = y // 50 
+y = y % 50
+a6 = y // 20
+y = y % 20
+a7 = y // 10
+y = y % 10
+a8 = y // 5
+y = y % 5 
+a9 = y // 1 
+y = y % 1
+print("1000 : ",a1)
+print("500: ",a2)
+print("200 : ",a3)
+print("100 : ",a4)
+print("50 : ",a5)
+print("20 : ",a6)
+print("10 : ",a7)
+print("5 : ",a8)
+print("1 : ",a9)
