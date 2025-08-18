f7ad07e4ad6da15e4cb1142a80f4b07ef2fc83b2
med4rda <paulomoral41@gmail.com>
2025-08-18 15:50:00 +0800

    Add files via upload

---
+++ MORAL/Activity9.py
@@ -0,0 +1,12 @@
+a = 5
+b = 10
+c = 15
+d = 20
+username = "paulopogi123"
+password = "paulogoodboy"
+print(username == password)
+print(username != password)
+print(a > b) and (c > d)
+print(a < b) and (c < d)
+print(not((a < b) and (c < d)))
+print(a != b) and (c != d)
