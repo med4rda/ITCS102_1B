436c1bc8e7f3575dd76822c2847f25eeaaa32fbe
Moral, Jose Paulo <paulomoral41@gmail.com>
2025-09-16 11:00:05 +0800

    Add files via upload

---
+++ MORAL/activity10.py
@@ -0,0 +1,10 @@
+username = "paulopogi"
+password = "loveumwah"
+
+uname = input("Enter username: ")
+pword = input("Enter password: ")
+
+if uname == username and pword == password:
+    print("ACCESS GRANTED")
+else:   
+    print("ACCESS DENIED")
