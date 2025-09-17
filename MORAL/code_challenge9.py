264ed8da24b2caa92777937344cb3b5f4e68c5f6
Moral, Jose Paulo <paulomoral41@gmail.com>
2025-09-17 17:15:09 +0800

    Add files via upload

---
+++ MORAL/code_challenge9.py
@@ -0,0 +1,5 @@
+print("🦜 PARROT SIMULATOR – THE ECHO CHAMBER!")
+ask = str(input("What do you want your parrot to say? "))
+times = eval(input("How many times should the parrot squawk it? "))
+for x in range(times,0,-1):
+    print("🦜 Squawk!",ask)      
