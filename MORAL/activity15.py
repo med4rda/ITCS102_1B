0711fcaf345cd7e69aea857578259a7d7ea292f3
Moral, Jose Paulo <paulomoral41@gmail.com>
2025-10-09 22:18:56 +0800

    Add files via upload

---
+++ MORAL/activity15.py
@@ -0,0 +1,8 @@
+f_name = "Jose Paulo"
+m_name = "Vergara"
+l_name = "Moral"
+house = "Python"
+sy = "2024 - 2025"
+full_name = f"{f_name} {m_name} {l_name}" 
+
+print(f"My name is {full_name}, from the house of {house}, year: {sy}")
