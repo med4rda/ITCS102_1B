bf41217b9d02fad3fa495cb6d444909d27fffd9b
Moral, Jose Paulo <paulomoral41@gmail.com>
2025-10-09 22:39:44 +0800

    Add files via upload

--- MORAL/code_challenge10.py
+++ MORAL/code_challenge10.py
@@ -1,4 +1,5 @@
-for x in range(11,1,-1):
-    for y in range(1, x ,1):
-        print("*", end=" ")
+print("MY PIRATE INVERTED RIGHT TRIANGLE!!") 
+for i in range(20,0,-1):
+    for pirate in range(i,0,-1):
+        print("☠️", end=" ") 
     print()
