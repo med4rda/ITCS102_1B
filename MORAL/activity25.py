b50977cbdc93da31d38dbd0ce1f528f236834722
Moral, Jose Paulo <paulomoral41@gmail.com>
2025-12-08 13:17:44 +0800

    Add files via upload

---
+++ MORAL/activity25.py
@@ -0,0 +1,22 @@
+#Dictionary
+prog_lang = ['Java', ' C#', 'C++', 'Python', ' Perl', 'Go Lang', 'Java Script']
+
+#Index          0       1     2        3         4        5             6  
+print(prog_lang[-1])
+
+# {Keys : Values, Keys : Values}    
+
+prog_dictionary = {'easy':'Java', 'medium':' C#', 'hard':'C++', 'small':'Python', 'sakto':' Perl', 'large':'Go Lang', 'super':'Java Script'}
+
+print(prog_dictionary['sakto'])
+
+#Adding item to a dictionary
+
+prog_dictionary['old'] = 'html'
+
+print(prog_dictionary)
+
+#Removing item to a dictionary
+
+# prog_dictionary.pop('keys')
+# print(prog_dictionary)
